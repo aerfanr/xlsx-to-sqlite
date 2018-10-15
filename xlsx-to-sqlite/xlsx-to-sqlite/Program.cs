87b573ff4c8c4545a01f7eac6a7fe7f36cc4d83f
erfan <erfan.rafati@outlook.com>
2018-10-15 20:34:14 +0330

    excel connection

--- xlsx-to-sqlite/xlsx-to-sqlite/Program.cs
+++ xlsx-to-sqlite/xlsx-to-sqlite/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace xlsx_to_sqlite
 {
@@ -10,6 +11,16 @@ namespace xlsx_to_sqlite
     {
         static void Main(string[] args)
         {
+            Excel.Application exApp = new Excel.Application();
+
+            Console.WriteLine("Please enter the full path of excel workbook");
+            string bookDir = Console.ReadLine();
+
+            Excel.Workbook exBook = exApp.Workbooks.Open(@bookDir);
+            Excel.Worksheet exSheet = exBook.Sheets[1];
+            Excel.Range exRange = exSheet.UsedRange;
+
+            Console.ReadLine();
         }
     }
 }
